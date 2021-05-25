c7b3c3b6a183ad05f9b998c19fc80675d2e41e8d
merik <merikbest2015@gmail.com>
2021-05-25 23:10:59 +0300

    twitter clone

---
+++ src/main/java/com/gmail/merikbest2015/twitterspringreactjs/TwitterSpringReactjsApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.merikbest2015.twitterspringreactjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterSpringReactjsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterSpringReactjsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gmail/merikbest2015/twitterspringreactjs/TwitterSpringReactjsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.merikbest2015.twitterspringreactjs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterSpringReactjsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
