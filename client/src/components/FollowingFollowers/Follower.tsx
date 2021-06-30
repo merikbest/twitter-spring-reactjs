ca7b57d249c4dc04489ff1a91ab0c4fa71034162
merik <merikbest2015@gmail.com>
2021-06-30 23:35:10 +0300

    Added spinner when followers loading. Added modal window when unfollow user.

--- client/src/components/FollowingFollowers/Follower.tsx
+++ client/src/components/FollowingFollowers/Follower.tsx
@@ -1,9 +1,12 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
+import {Link, useHistory} from 'react-router-dom';
 import Paper from '@material-ui/core/Paper';
 import {Avatar, Button, Typography} from "@material-ui/core";
 
 import {User} from "../../store/ducks/user/contracts/state";
 import {useStylesFollower} from "./FollowerStyles";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
 
 interface FollowerProps {
     user: User
@@ -11,26 +14,69 @@ interface FollowerProps {
 
 const Follower: FC<FollowerProps> = ({user}) => {
     const classes = useStylesFollower();
+    const history = useHistory();
+    const [btnText, setBtnText] = useState<string>("Following");
+    const [visibleUnfollowModal, setVisibleUnfollowModal] = useState<boolean>(false);
+
+    const handleClickOpenUnfollowModal = (): void => {
+        setVisibleUnfollowModal(true);
+    };
+
+    const onCloseUnfollowModal = (): void => {
+        setVisibleUnfollowModal(false);
+    };
+
+    const onClick = (event: React.MouseEvent<HTMLAnchorElement>): void => {
+        event.preventDefault();
+        if (!visibleUnfollowModal) {
+            history.push(`/user/${user.id}`);
+        }
+    };
 
     return (
         <Paper className={classes.follower} variant="outlined">
-            <Avatar className={classes.followerAvatar} src={user?.avatar?.src ? user?.avatar.src :
-                "https://abs.twimg.com/sticky/default_profile_images/default_profile_reasonably_small.png"}/>
+            <Link to={`/user/${user.id}`} className={classes.followerLink}>
+                <Avatar className={classes.followerAvatar} src={user?.avatar?.src ? user?.avatar.src :
+                    "https://abs.twimg.com/sticky/default_profile_images/default_profile_reasonably_small.png"}/>
+            </Link>
             <div style={{flex: 1}}>
                 <div className={classes.followerWrapper}>
+                    <Link to={`/user/${user.id}`} className={classes.followerLink}>
+                        <div style={{width: 350}}>
+                            <Typography className={classes.followerFullName}>
+                                {user?.fullName}
+                            </Typography>
+                            <Typography className={classes.followerUsername} variant="caption" display="block" gutterBottom>
+                                @{user?.username}
+                            </Typography>
+                        </div>
+                    </Link>
                     <div>
-                        <Typography className={classes.followerFullName}>
-                            {user?.fullName}
-                        </Typography>
-                        <Typography className={classes.followerUsername} variant="caption" display="block" gutterBottom>
-                            @{user?.username}
-                        </Typography>
-                    </div>
-                    <div>
-                        <Button variant="contained" color="primary" className={classes.followerBtn}>Following</Button>
+                        <Button
+                            className={classes.followerBtn}
+                            onMouseOver={() => setBtnText("Unfollow")}
+                            onMouseLeave={() => setBtnText("Following")}
+                            onClick={handleClickOpenUnfollowModal}
+                            variant="contained"
+                            color="primary">
+                            {btnText}
+                        </Button>
                     </div>
                 </div>
                 <Typography display="block">{user?.about}</Typography>
+                <Dialog open={visibleUnfollowModal} onClose={onCloseUnfollowModal} aria-labelledby="form-dialog-title">
+                    <DialogContent style={{padding: "0px 0px"}}>
+                        <div style={{width: 280, height: 176, textAlign: "center", margin: "32px 20px"}}>
+                            <Typography className={classes.followerFullName}>
+                                Unfollow {user?.fullName} ?
+                            </Typography>
+                            <Typography className={classes.followerUsername} variant="caption" display="block" gutterBottom>
+                                Their Tweets will no longer show up in your home timeline. You can still view their
+                                profile, unless their Tweets are protected.
+                            </Typography>
+                        </div>
+                    </DialogContent>
+                </Dialog>
             </div>
         </Paper>
     );
--- client/src/components/FollowingFollowers/FollowingFollowers.tsx
+++ client/src/components/FollowingFollowers/FollowingFollowers.tsx
@@ -1,25 +1,26 @@
 import React, {ChangeEvent, FC, useEffect, useState} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import Paper from '@material-ui/core/Paper';
-import {Typography} from "@material-ui/core";
+import {Button, CircularProgress, Menu, MenuItem, Typography} from "@material-ui/core";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 
 import {BackButton} from "../BackButton/BackButton";
-import {useHomeStyles} from "../../pages/Home/HomeStyles";
 import {selectUser} from "../../store/ducks/users/selectors";
-import {selectUserFollowers} from "../../store/ducks/user/selectors";
+import {selectUserFollowers, selectUserIsLoading} from "../../store/ducks/user/selectors";
 import Follower from "./Follower";
-import {useLocation, useParams} from "react-router-dom";
+import {Link, useLocation, useParams} from "react-router-dom";
 import {fetchUserFollowers, fetchUserFollowing} from "../../store/ducks/user/actionCreators";
+import {useStylesFollower} from "./FollowerStyles";
 
 const FollowingFollowers = () => {
-    const classes = useHomeStyles();
+    const classes = useStylesFollower();
     const dispatch = useDispatch();
     const location = useLocation<{ follow: number }>();
     const params = useParams<{ id: string }>();
     const userProfile = useSelector(selectUser);
     const followers = useSelector(selectUserFollowers);
+    const isFollowersLoading = useSelector(selectUserIsLoading);
     const [activeTab, setActiveTab] = useState<number>(0);
 
     useEffect(() => {
@@ -44,8 +45,8 @@ const FollowingFollowers = () => {
     };
 
     return (
-        <Paper className={classes.tweetsWrapper} variant="outlined">
-            <Paper className={classes.tweetsHeader}>
+        <Paper className={classes.followersWrapper} variant="outlined">
+            <Paper className={classes.followersHeader}>
                 <BackButton/>
                 <div>
                     <Typography variant="h6">{userProfile?.fullName}</Typography>
@@ -53,10 +54,39 @@ const FollowingFollowers = () => {
                 </div>
             </Paper>
             <Tabs value={activeTab} indicatorColor="primary" textColor="primary" onChange={handleChangeTab}>
-                <Tab onClick={handleShowFollowing} style={{minWidth: "301px", textTransform: 'none'}} label="Following"/>
-                <Tab onClick={handleShowFollowers} style={{minWidth: "301px", textTransform: 'none'}} label="Followers"/>
+                <Tab onClick={handleShowFollowing} className={classes.followersTab} label="Following"/>
+                <Tab onClick={handleShowFollowers} className={classes.followersTab} label="Followers"/>
             </Tabs>
-            {followers ? (followers.map((user) => <Follower user={user}/>)) : null}
+            {isFollowersLoading ? (
+                <div className={classes.followersCentred}>
+                    <CircularProgress/>
+                </div>
+            ) : (followers?.length !== 0 ? (
+                    followers?.map((user) => <Follower user={user}/>)
+                ) : (activeTab === 0 ? (
+                        <div className={classes.followersTopicWrapper}>
+                            <Typography className={classes.followersTopic}>
+                                You aren’t following anyone yet
+                            </Typography>
+                            <Typography className={classes.followersText}>
+                                When you do, they’ll be listed here and you’ll see their Tweets in your timeline.
+                            </Typography>
+                            <Button variant="contained" color="primary">
+                                Find people to follow
+                            </Button>
+                        </div>
+                    ) : (
+                        <div className={classes.followersTopicWrapper}>
+                            <Typography className={classes.followersTopic}>
+                                You don’t have any followers yet
+                            </Typography>
+                            <Typography className={classes.followersText}>
+                                When someone follows you, you’ll see them here.
+                            </Typography>
+                        </div>
+                    )
+                )
+            )}
         </Paper>
     );
 };
