137d40aad37e338ae608906136ad61903342297f
merik <merikbest2015@gmail.com>
2021-10-27 23:39:19 +0300

    Update TweetDeckTeams component and style.

--- client/src/pages/Settings/Account/TweetDeckTeams/TweetDeckTeams.tsx
+++ client/src/pages/Settings/Account/TweetDeckTeams/TweetDeckTeams.tsx
@@ -1,11 +1,16 @@
-import React, {FC, ReactElement} from 'react';
-import {Paper, Typography} from "@material-ui/core";
+import React, {FC, ReactElement, useState} from 'react';
+import {Paper, Radio, Switch, Typography} from "@material-ui/core";
 
 import {useTweetDeckTeamsStyles} from "./TweetDeckTeamsStyles";
+import {CheckCircle, RadioButtonUnchecked} from "@material-ui/icons";
 
 const TweetDeckTeams: FC = (): ReactElement => {
     const classes = useTweetDeckTeamsStyles();
+    const [selectedValue, setSelectedValue] = useState<string>("Anyone");
 
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        setSelectedValue(event.target.value);
+    };
     return (
         <div className={classes.container}>
             <Paper variant="outlined">
@@ -16,7 +21,50 @@ const TweetDeckTeams: FC = (): ReactElement => {
                         </Typography>
                     </div>
                     <div className={classes.infoItemWrapper}>
-
+                        <Typography component={"div"} className={classes.title}>
+                            Turn on TweetDeck Teams
+                            <span className={classes.switch}>
+                                <Switch defaultChecked />
+                            </span>
+                        </Typography>
+                        <Typography component={"div"} className={classes.text}>
+                            When this setting is on, you can invite anyone to Tweet from this account using TweetDeck
+                            Teams. <a href={"https://help.twitter.com/using-twitter/tweetdeck-teams"} target="_blank"
+                                      className={classes.link}>Learn more</a>
+                        </Typography>
+                    </div>
+                    <div className={classes.divider}/>
+                    <div className={classes.infoItemWrapper}>
+                        <div className={classes.tweetDeckItemWrapper}>
+                            <Typography component={"span"}>
+                                Allow anyone to add you to their team
+                            </Typography>
+                            <Radio
+                                checked={selectedValue === "Anyone"}
+                                onChange={handleChange}
+                                value="Anyone"
+                                name="radio-buttons"
+                                inputProps={{"aria-label": "Anyone"}}
+                                icon={<RadioButtonUnchecked color={"primary"}/>}
+                                checkedIcon={<CheckCircle color={"primary"}/>}
+                                size="small"
+                            />
+                        </div>
+                        <div className={classes.tweetDeckItemWrapper}>
+                            <Typography component={"span"}>
+                                Only allow people you follow to add you to their team
+                            </Typography>
+                            <Radio
+                                checked={selectedValue === "Others"}
+                                onChange={handleChange}
+                                value="Others"
+                                name="radio-buttons"
+                                inputProps={{"aria-label": "Others"}}
+                                icon={<RadioButtonUnchecked color={"primary"}/>}
+                                checkedIcon={<CheckCircle color={"primary"}/>}
+                                size="small"
+                            />
+                        </div>
                     </div>
                 </div>
             </Paper>
--- client/src/pages/Settings/Account/TweetDeckTeams/TweetDeckTeamsStyles.tsx
+++ client/src/pages/Settings/Account/TweetDeckTeams/TweetDeckTeamsStyles.tsx
@@ -24,4 +24,50 @@ export const useTweetDeckTeamsStyles = makeStyles((theme: Theme) => ({
         fontSize: 13,
         lineHeight: "16px"
     },
+    title: {
+        paddingBottom: 4,
+        fontWeight: 700,
+        fontSize: 15,
+        color: "rgb(15, 20, 25)",
+        lineHeight: "20px"
+    },
+    switch: {
+        float: "right",
+    },
+    link: {
+        textDecoration: "none",
+        color: "rgb(29, 155, 240)",
+        cursor: "pointer",
+        "&:hover": {
+            textDecoration: "underline",
+        }
+    },
+    divider: {
+        height: 1,
+        backgroundColor: "rgb(239, 243, 244)",
+    },
+    checklistText: {
+        padding: "4px 0px",
+        color: "rgb(15, 20, 25)",
+        fontWeight: 400,
+        fontSize: 15,
+        lineHeight: "20px"
+    },
+    tweetDeckItemWrapper: {
+        padding: "4px 0px",
+        "& .MuiTypography-root": {
+            fontSize: 15,
+            color: "rgb(15, 20, 25)",
+            fontWeight: 400,
+            lineHeight: "20px",
+        },
+        "& .MuiButtonBase-root": {
+            padding: 4,
+            float: "right",
+            "& .MuiSvgIcon-root": {
+                width: 20,
+                height: 20
+            },
+        },
+    },
 }));
