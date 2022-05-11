35f7018432b46fc2d11cdf4073f05717297b8864
merikbest <merikbest2015@gmail.com>
2022-05-11 20:42:36 +0300

    Added TrendsItem tests.

---
+++ client/src/pages/Trends/TrendsItem/__tests__/TrendsItem.test.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import {createMemoryHistory} from "history";
+import {ListItemText} from "@material-ui/core";
+
+import TrendsItem from "../TrendsItem";
+import {createMockRootState, mountWithStore} from "../../../../util/testHelper";
+import {mockTags} from "../../../../util/mockData/mockData";
+
+describe("TrendsItem", () => {
+
+    it("should render correctly", () => {
+        const history = createMemoryHistory();
+        const pushSpy = jest.spyOn(history, "push");
+        const mockTag = mockTags[0];
+        const wrapper = mountWithStore(<TrendsItem tag={mockTag}/>, createMockRootState(), history);
+
+        wrapper.find(Link).at(0).simulate("click", {button: 0});
+
+        expect(pushSpy).toHaveBeenCalled();
+        expect(pushSpy).toHaveBeenCalledWith({pathname: "/search", state: {tag: mockTag.tagName}});
+        expect(wrapper.find(ListItemText).prop("primary")).toBe(mockTag.tagName);
+        expect(wrapper.text().includes(`${mockTag.tweetsQuantity} Tweets`)).toBe(true);
+    });
+});
