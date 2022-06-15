147dd65679c7852e09e06aafc529d99b22aa0c1e
mkhotynskyi <mkhotynskyi@magnite.com>
2022-06-15 22:02:42 +0300

    Added AddTweetModal and BackButton tests.

---
+++ client/src/components/AddTweetModal/__tests__/AddTweetModal.test.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {Dialog} from "@material-ui/core";
+
+import {createMockRootState, mountWithStore} from "../../../util/testHelper";
+import AddTweetForm from "../../AddTweetForm/AddTweetForm";
+import AddTweetModal from "../AddTweetModal";
+import {LoadingStatus} from "../../../store/types";
+
+describe("AddTweetModal", () => {
+    const mockRootState = createMockRootState(LoadingStatus.LOADED);
+    
+    it("should render correctly", () => {
+        const mockOnClose = jest.fn();
+        const wrapper = mountWithStore(<AddTweetModal visible={true} onClose={mockOnClose}/>, mockRootState);
+        
+        expect(mockOnClose).toHaveBeenCalled();
+        expect(wrapper.find(Dialog).exists()).toBeTruthy();
+        expect(wrapper.find(AddTweetForm).prop("title")).toBe("What's happening?");
+        expect(wrapper.find(AddTweetForm).prop("buttonName")).toBe("Tweet");
+    });
+
+    it("should render empty Add Tweet Modal correctly", () => {
+        const mockOnClose = jest.fn();
+        const wrapper = mountWithStore(<AddTweetModal visible={false} onClose={mockOnClose}/>, mockRootState);
+
+        expect(wrapper.find(Dialog).exists()).toBeFalsy();
+    });
+});
