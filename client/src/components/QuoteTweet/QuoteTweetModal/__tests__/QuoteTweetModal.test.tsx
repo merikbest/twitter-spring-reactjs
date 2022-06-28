f46026e39a7f641fcd89951ae92d212b65a70fe8
merikbest <merikbest2015@gmail.com>
2022-06-28 15:10:56 +0300

    Added QuoteTweetModal and ReplyModal tests.

---
+++ client/src/components/QuoteTweet/QuoteTweetModal/__tests__/QuoteTweetModal.test.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {Dialog} from "@material-ui/core";
+
+import {createMockRootState, mountWithStore} from "../../../../util/testHelper";
+import {LoadingStatus} from "../../../../store/types";
+import {mockQuoteTweet} from "../../../../util/mockData/mockData";
+import QuoteTweetModal from "../QuoteTweetModal";
+import CloseButton from "../../../CloseButton/CloseButton";
+import AddTweetForm from "../../../AddTweetForm/AddTweetForm";
+
+describe("QuoteTweetModal", () => {
+    const mockRootState = createMockRootState(LoadingStatus.LOADED);
+
+    it("should render correctly", () => {
+        const wrapper = mountWithStore(
+            <QuoteTweetModal
+                quoteTweet={mockQuoteTweet}
+                visible={true}
+                onClose={jest.fn()}
+            />, mockRootState);
+
+        expect(wrapper.find(Dialog).exists()).toBeTruthy();
+        expect(wrapper.find(CloseButton).exists()).toBeTruthy();
+        expect(wrapper.find(AddTweetForm).exists()).toBeTruthy();
+    });
+
+    it("should render empty QuoteTweetModal", () => {
+        const wrapper = mountWithStore(
+            <QuoteTweetModal
+                quoteTweet={mockQuoteTweet}
+                visible={false}
+                onClose={jest.fn()}
+            />, mockRootState);
+
+        expect(wrapper.find(Dialog).exists()).toBeFalsy();
+    });
+});
---
+++ client/src/components/ReplyModal/__tests__/ReplyModal.test.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {Avatar, Dialog} from "@material-ui/core";
+
+import {createMockRootState, mountWithStore} from "../../../util/testHelper";
+import {LoadingStatus} from "../../../store/types";
+import {mockMyFullTweet} from "../../../util/mockData/mockData";
+import {Image} from "../../../store/types/common";
+import ReplyModal from "../ReplyModal";
+
+describe("ReplyModal", () => {
+    const mockRootState = createMockRootState(LoadingStatus.LOADED);
+
+    it("should render correctly", () => {
+        const wrapper = createReplyModalWrapper();
+        expect(wrapper.find(Avatar).at(0).prop("src")).toBe(mockMyFullTweet.user.avatar.src);
+        expect(wrapper.text().includes(mockMyFullTweet.user.fullName)).toBe(true);
+        expect(wrapper.text().includes(mockMyFullTweet.user.username)).toBe(true);
+        expect(wrapper.text().includes("Mar 22")).toBe(true);
+        expect(wrapper.text().includes("hello23")).toBe(true);
+    });
+
+    it("should render with image", () => {
+        const wrapper = createReplyModalWrapper(true, mockMyFullTweet.user.avatar);
+        expect(wrapper.find("img").at(1).prop("src")).toBe(mockMyFullTweet.user.avatar.src);
+    });
+
+    it("should render empty ReplyModal", () => {
+        const wrapper = createReplyModalWrapper(false);
+        expect(wrapper.find(Dialog).exists()).toBeFalsy();
+    });
+
+    const createReplyModalWrapper = (visible = true, image?: Image) => {
+        return mountWithStore(
+            <ReplyModal
+                user={mockMyFullTweet.user}
+                tweetId={1}
+                text={mockMyFullTweet.text}
+                image={image}
+                dateTime={mockMyFullTweet.dateTime}
+                visible={visible}
+                onClose={jest.fn()}
+            />, mockRootState);
+    };
+});
