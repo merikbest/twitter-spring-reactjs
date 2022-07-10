875df5923ac9a2a4ba9b14c48e003bf31842bb1f
merikbest <merikbest2015@gmail.com>
2022-07-10 14:03:36 +0300

    Added UnfollowModal tests.

---
+++ client/src/components/UnfollowModal/__tests__/UnfollowModal.test.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {Button, Dialog} from "@material-ui/core";
+
+import {createMockRootState, mountWithStore} from "../../../util/testHelper";
+import {LoadingStatus} from "../../../store/types";
+import {mockUsers} from "../../../util/mockData/mockData";
+import UnfollowModal from "../UnfollowModal";
+
+describe("UnfollowModal", () => {
+    const mockRootState = createMockRootState(LoadingStatus.SUCCESS);
+
+    it("should render correctly", () => {
+        const wrapper = mountWithStore(
+            <UnfollowModal 
+                user={mockUsers[0]}
+                visible={true}
+                onClose={jest.fn()}
+                handleUnfollow={jest.fn()}
+            />, mockRootState);
+        
+        wrapper.find(Dialog).simulate("click");
+        
+        expect(wrapper.text().includes(`Unfollow ${mockUsers[0].fullName}`)).toBe(true);
+        expect(wrapper.text().includes("Their Tweets will no longer show up in your home timeline.")).toBe(true);
+        expect(wrapper.find(Button).at(0).text().includes("Cancel")).toBe(true);
+        expect(wrapper.find(Button).at(1).text().includes("Unfollow")).toBe(true);
+    });
+
+    it("should render empty UnfollowModal", () => {
+        const wrapper = mountWithStore(
+            <UnfollowModal
+                user={mockUsers[0]}
+                visible={false}
+                onClose={jest.fn()}
+                handleUnfollow={jest.fn()}
+            />, mockRootState);
+
+        expect(wrapper.find(Dialog).exists()).toBeFalsy();
+    });
+});
