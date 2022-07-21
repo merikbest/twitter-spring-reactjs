7b24ad7edab6fcd6b8cc305f82f9d7b5630d0fb7
merikbest <merikbest2015@gmail.com>
2022-07-21 22:15:17 +0300

    Added Welcome tests.

---
+++ client/src/components/Welcome/__tests__/Welcome.test.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {Button} from "@material-ui/core";
+
+import {createMockRootState, mockDispatch, mountWithStore} from "../../../util/testHelper";
+import {LoadingStatus} from "../../../store/types";
+import {UserActionsType} from "../../../store/ducks/user/contracts/actionTypes";
+import Welcome from "../Welcome";
+
+describe("Welcome", () => {
+    const mockRootState = createMockRootState(LoadingStatus.SUCCESS);
+    let mockDispatchFn: jest.Mock;
+
+    beforeEach(() => {
+        mockDispatchFn = mockDispatch();
+    });
+
+    it("should render correctly", () => {
+        const wrapper = mountWithStore(<Welcome/>, mockRootState);
+        expect(wrapper.text().includes("Welcome to Twitter!")).toBe(true);
+        expect(wrapper.text().includes("This is the best place to see what’s happening in your world.")).toBe(true);
+        expect(wrapper.find(Button).text().includes("Let's go")).toBe(true);
+    });
+
+    it("should click start use Twitter", () => {
+        const wrapper = mountWithStore(<Welcome/>, mockRootState);
+        wrapper.find(Button).simulate("click");
+        expect(mockDispatchFn).nthCalledWith(1, {payload: 2, type: UserActionsType.START_USE_TWITTER});
+    });
+});
