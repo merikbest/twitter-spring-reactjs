a1bfc42ced325bd787b7fc3ea57a85df0c0dad36
merikbest <merikbest2015@gmail.com>
2022-08-18 21:03:44 +0300

    Added pagination to MessagesModal component.

--- client/src/pages/Messages/MessagesModal/MessagesModal.tsx
+++ client/src/pages/Messages/MessagesModal/MessagesModal.tsx
@@ -6,14 +6,19 @@ import DialogContent from "@material-ui/core/DialogContent";
 
 import {useMessagesModalStyles} from "./MessagesModalStyles";
 import {MessagesModalInput} from "./MessagesModalInput/MessagesModalInput"
-import {fetchUsersSearchByUsername, setUsersSearch} from "../../../store/ducks/usersSearch/actionCreators";
-import {selectUsersSearch} from "../../../store/ducks/usersSearch/selectors";
+import {
+    fetchUsersSearchByUsername,
+    resetUsersState,
+    setUsersSearch
+} from "../../../store/ducks/usersSearch/actionCreators";
+import {selectUsersPagesCount, selectUsersSearch} from "../../../store/ducks/usersSearch/selectors";
 import MessagesModalUser from './MessagesModalUser/MessagesModalUser';
 import {createChat} from "../../../store/ducks/chats/actionCreators";
 import {SearchIcon} from "../../../icons";
 import CloseButton from "../../../components/CloseButton/CloseButton";
 import {selectUserData} from "../../../store/ducks/user/selectors";
 import {UserResponse} from "../../../store/types/user";
+import InfiniteScrollWrapper from '../../../components/InfiniteScrollWrapper/InfiniteScrollWrapper';
 
 interface MessagesModalProps {
     visible?: boolean;
@@ -25,11 +30,12 @@ const MessagesModal: FC<MessagesModalProps> = ({visible, onClose}): ReactElement
     const dispatch = useDispatch();
     const users = useSelector(selectUsersSearch);
     const myProfile = useSelector(selectUserData);
+    const usersPagesCount = useSelector(selectUsersPagesCount);
     const [text, setText] = useState<string>("");
     const [selectedIndex, setSelectedIndex] = useState<number>();
     const [selectedUser, setSelectedUser] = useState<UserResponse>();
 
-    const handleClickSearch = (event: FormEvent<HTMLFormElement>): void => {
+    const handleSubmitSearch = (event: FormEvent<HTMLFormElement>): void => {
         event.preventDefault();
         dispatch(fetchUsersSearchByUsername({ username: encodeURIComponent(text), page: 0 })); // TODO add <InfiniteScroll/>
     };
@@ -37,11 +43,16 @@ const MessagesModal: FC<MessagesModalProps> = ({visible, onClose}): ReactElement
     const onSearch = (text: string): void => {
         if (text) {
             setText(text);
-            dispatch(fetchUsersSearchByUsername({ username: encodeURIComponent(text), page: 0 })); // TODO add <InfiniteScroll/>
+            dispatch(resetUsersState());
+            loadParticipants(0);
         } else {
             setText("");
             dispatch(setUsersSearch([]));
         }
+    };
+
+    const loadParticipants = (page: number): void => {
+        dispatch(fetchUsersSearchByUsername({ username: encodeURIComponent(text), page }));
     };
 
     const handleClickAddUserToChat = (): void => {
@@ -83,37 +94,43 @@ const MessagesModal: FC<MessagesModalProps> = ({visible, onClose}): ReactElement
                     Next
                 </Button>
             </DialogTitle>
-            <DialogContent className={classes.content}>
-                <form onSubmit={handleClickSearch}>
-                    <MessagesModalInput
-                        fullWidth
-                        placeholder="Explore people"
-                        variant="outlined"
-                        onChange={(event) => onSearch(event.target.value)}
-                        value={text}
-                        InputProps={{
-                            startAdornment: (
-                                <InputAdornment position="start">
-                                    {SearchIcon}
-                                </InputAdornment>
-                            ),
-                        }}
-                    />
-                </form>
-                <Divider/>
-                <List component="nav" aria-label="main mailbox folders">
-                    {users.map((user) => (
-                        <ListItem
-                            key={user.id}
-                            button
-                            selected={selectedIndex === user.id!}
-                            disabled={user.isMutedDirectMessages || user.id === myProfile?.id}
-                            onClick={() => handleListItemClick(user)}
-                        >
-                            <MessagesModalUser user={user}/>
-                        </ListItem>
-                    ))}
-                </List>
+            <DialogContent id="scrollableDiv" className={classes.content}>
+                <InfiniteScrollWrapper
+                    dataLength={users.length}
+                    pagesCount={usersPagesCount}
+                    loadItems={loadParticipants}
+                >
+                    <form onSubmit={handleSubmitSearch}>
+                        <MessagesModalInput
+                            fullWidth
+                            placeholder="Explore people"
+                            variant="outlined"
+                            onChange={(event) => onSearch(event.target.value)}
+                            value={text}
+                            InputProps={{
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        {SearchIcon}
+                                    </InputAdornment>
+                                ),
+                            }}
+                        />
+                    </form>
+                    <Divider/>
+                    <List component="nav" aria-label="main mailbox folders">
+                        {users.map((user) => (
+                            <ListItem
+                                key={user.id}
+                                button
+                                selected={selectedIndex === user.id!}
+                                disabled={user.isMutedDirectMessages || user.id === myProfile?.id}
+                                onClick={() => handleListItemClick(user)}
+                            >
+                                <MessagesModalUser user={user}/>
+                            </ListItem>
+                        ))}
+                    </List>
+                </InfiniteScrollWrapper>
             </DialogContent>
         </Dialog>
     );
