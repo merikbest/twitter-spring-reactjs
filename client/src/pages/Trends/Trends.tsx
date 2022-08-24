50e3add4c1714a0657a9d0901949cb949f78aabb
merikbest <merikbest2015@gmail.com>
2022-08-24 22:03:18 +0300

    Added pagination to Trends page.

--- client/src/pages/Trends/Trends.tsx
+++ client/src/pages/Trends/Trends.tsx
@@ -3,35 +3,42 @@ import {useDispatch, useSelector} from "react-redux";
 import {List} from "@material-ui/core";
 
 import TrendsItem from "./TrendsItem/TrendsItem";
-import {selectIsTrendsLoading, selectTrendsItems} from "../../store/ducks/tags/selectors";
+import {selectIsTrendsLoading, selectTrendsItems, selectTrendsPagesCount} from "../../store/ducks/tags/selectors";
 import {fetchTrends, resetTrendsState} from "../../store/ducks/tags/actionCreators";
 import Spinner from "../../components/Spinner/Spinner";
 import {withDocumentTitle} from "../../hoc/withDocumentTitle";
+import InfiniteScrollWrapper from '../../components/InfiniteScrollWrapper/InfiniteScrollWrapper';
 
 const Trends: FC = (): ReactElement => {
     const dispatch = useDispatch();
     const isLoading = useSelector(selectIsTrendsLoading);
     const trends = useSelector(selectTrendsItems);
+    const pagesCount = useSelector(selectTrendsPagesCount);
 
     useEffect(() => {
         window.scrollTo(0, 0);
-        dispatch(fetchTrends(0));
+        loadTrends(0);
 
         return () => {
             dispatch(resetTrendsState());
         };
     }, []);
 
+    const loadTrends = (page: number): void => {
+        dispatch(fetchTrends(page));
+    };
+
     return (
-        <div>
-            {isLoading ? (
+        <InfiniteScrollWrapper dataLength={trends.length} pagesCount={pagesCount} loadItems={loadTrends}>
+            {isLoading && !trends.length ? (
                 <Spinner paddingTop={80}/>
             ) : (
                 <List style={{paddingTop: 48}}>
                     {trends.map((trend) => <TrendsItem key={trend.id} tag={trend}/>)}
+                    {isLoading && <Spinner/>}
                 </List>
             )}
-        </div>
+        </InfiniteScrollWrapper>
     );
 };
 
