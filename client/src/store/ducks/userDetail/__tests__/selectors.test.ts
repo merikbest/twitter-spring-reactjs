f7cb0aa8e3ff9b3c8c65abbbbf4a95f7ee0f5fcc
merikbest <merikbest2015@gmail.com>
2022-11-24 03:07:26 +0200

    Added userDetails redux tests.

--- client/src/store/ducks/userDetail/__tests__/selectors.test.ts
+++ client/src/store/ducks/userDetail/__tests__/selectors.test.ts
@@ -1,12 +1,108 @@
-import {selectIsUserDetailLoaded, selectIsUserDetailLoading, selectUserDetailItem} from "../selectors";
-import {createMockRootState, mockUserDetailResponse} from "../../../../util/testHelper";
+import {
+    selectIsUserDetailLoaded,
+    selectIsUserDetailLoading,
+    selectUserDetailAbout,
+    selectUserDetailAvatar,
+    selectUserDetailFollowersSize,
+    selectUserDetailFollowingSize,
+    selectUserDetailFullName,
+    selectUserDetailId,
+    selectUserDetailIsFollower,
+    selectUserDetailIsMyProfileBlocked,
+    selectUserDetailIsPrivateProfile,
+    selectUserDetailIsUserBlocked,
+    selectUserDetailIsWaitingForApprove,
+    selectUserDetailItem,
+    selectUserDetailSameFollowers,
+    selectUserDetailUsername
+} from "../selectors";
+import {createMockRootState} from "../../../../util/testHelper";
 import {LoadingStatus} from "../../../types/common";
+import {mockUserDetailResponse} from "../../../../util/mockData/mockData";
 
 describe("userDetail selectors:", () => {
     
     describe("selectUserDetailItem", () => {
         it("should return UserDetailResponse", () => {
             expect(selectUserDetailItem(createMockRootState())).toBe(mockUserDetailResponse);
+        });
+    });
+
+    describe("selectUserDetailId", () => {
+        it("should return UserDetailId number", () => {
+            expect(selectUserDetailId(createMockRootState())).toBe(1);
+        });
+    });
+
+    describe("selectUserDetailUsername", () => {
+        it("should return UserDetailUsername string", () => {
+            expect(selectUserDetailUsername(createMockRootState())).toBe("Random");
+        });
+    });
+
+    describe("selectUserDetailFullName", () => {
+        it("should return UserDetailFullName string", () => {
+            expect(selectUserDetailFullName(createMockRootState())).toBe("Random");
+        });
+    });
+
+    describe("selectUserDetailAbout", () => {
+        it("should return UserDetailAbout string", () => {
+            expect(selectUserDetailAbout(createMockRootState())).toBe("About");
+        });
+    });
+
+    describe("selectUserDetailFollowersSize", () => {
+        it("should return UserDetailFollowersSize number", () => {
+            expect(selectUserDetailFollowersSize(createMockRootState())).toBe(2);
+        });
+    });
+
+    describe("selectUserDetailFollowingSize", () => {
+        it("should return UserDetailFollowingSize number", () => {
+            expect(selectUserDetailFollowingSize(createMockRootState())).toBe(4);
+        });
+    });
+
+    describe("selectUserDetailAvatar", () => {
+        it("should return UserDetailAvatar string", () => {
+            expect(selectUserDetailAvatar(createMockRootState())).toBe(mockUserDetailResponse.avatar.src);
+        });
+    });
+
+    describe("selectUserDetailIsMyProfileBlocked", () => {
+        it("should return isMyProfileBlocked boolean", () => {
+            expect(selectUserDetailIsMyProfileBlocked(createMockRootState())).toBe(false);
+        });
+    });
+
+    describe("selectUserDetailIsFollower", () => {
+        it("should return isFollower boolean", () => {
+            expect(selectUserDetailIsFollower(createMockRootState())).toBe(true);
+        });
+    });
+
+    describe("selectUserDetailIsUserBlocked", () => {
+        it("should return isUserBlocked boolean", () => {
+            expect(selectUserDetailIsUserBlocked(createMockRootState())).toBe(false);
+        });
+    });
+
+    describe("selectUserDetailIsWaitingForApprove", () => {
+        it("should return isWaitingForApprove boolean", () => {
+            expect(selectUserDetailIsWaitingForApprove(createMockRootState())).toBe(false);
+        });
+    });
+
+    describe("selectUserDetailIsPrivateProfile", () => {
+        it("should return isPrivateProfile boolean", () => {
+            expect(selectUserDetailIsPrivateProfile(createMockRootState())).toBe(false);
+        });
+    });
+
+    describe("selectUserDetailSameFollowers", () => {
+        it("should return sameFollowers array", () => {
+            expect(selectUserDetailSameFollowers(createMockRootState())).toBe(mockUserDetailResponse.sameFollowers);
         });
     });
 
