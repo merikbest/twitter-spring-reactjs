f34ea5857aff2b33e0d62ed7f2520911eb906e48
merikbest <merikbest2015@gmail.com>
2022-12-28 22:42:16 +0200

    Added NotInterested page tests.

---
+++ client/src/pages/Topics/NotInterested/__tests__/NotInterested.test.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+import {createMockRootState, mockDispatch, mountWithStore} from "../../../../util/testHelper";
+import {LoadingStatus} from "../../../../store/types/common";
+import {mockTopics} from "../../../../util/mockData/mockData";
+import Spinner from "../../../../components/Spinner/Spinner";
+import {TopicsActionsType} from "../../../../store/ducks/topics/contracts/actionTypes";
+import NotInterested from "../NotInterested";
+import TopicItem from "../../TopicItem/TopicItem";
+
+describe("NotInterested", () => {
+    const mockRootState = createMockRootState(LoadingStatus.LOADED);
+    let mockDispatchFn: jest.Mock;
+
+    beforeEach(() => {
+        mockDispatchFn = mockDispatch();
+    });
+
+    it("should render loading spinner", () => {
+        const wrapper = mountWithStore(<NotInterested/>, createMockRootState());
+        expect(wrapper.find(Spinner).exists()).toBe(true);
+        expect(mockDispatchFn).nthCalledWith(1, {type: TopicsActionsType.FETCH_NOT_INTERESTED_TOPICS});
+    });
+
+    it("should render EmptyPageDescription", () => {
+        const wrapper = mountWithStore(<NotInterested/>, mockRootState);
+        expect(wrapper.text().includes("No interest? No problem.")).toBe(true);
+    });
+
+    it("should render TopicItems", () => {
+        const mockState = {...mockRootState, topics: {...mockRootState.topics, topics: mockTopics}};
+        const wrapper = mountWithStore(<NotInterested/>, mockState);
+        expect(wrapper.find(TopicItem).length).toEqual(3);
+    });
+
+    it("should reset NotInterested page", () => {
+        const wrapper = mountWithStore(<NotInterested/>, mockRootState);
+        wrapper.unmount();
+        expect(mockDispatchFn).nthCalledWith(2, {type: TopicsActionsType.RESET_TOPICS_STATE});
+    });
+});
