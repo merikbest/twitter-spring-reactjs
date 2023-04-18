a3b6cc6387a2f4adfd3a4bcbe02dae637fd17744
merikbest <merikbest2015@gmail.com>
2023-04-18 14:30:45 +0300

    Added usePopup hook. Added image description modal window to the TweetImage component.

--- frontend/src/components/TweetComponent/TweetImage/TweetImage.tsx
+++ frontend/src/components/TweetComponent/TweetImage/TweetImage.tsx
@@ -1,8 +1,10 @@
 import React, { FC, memo, ReactElement } from "react";
 import { Link, useLocation } from "react-router-dom";
+import { Button, Popover, Typography } from "@material-ui/core";
 
 import { MODAL } from "../../../constants/path-constants";
 import { useTweetImageStyles } from "./TweetImageStyles";
+import { usePopup } from "../../../hook/usePopup";
 
 interface TweetImageProps {
     tweetId?: number;
@@ -13,13 +15,44 @@ const TweetImage: FC<TweetImageProps> = memo(({ tweetId, imageSrc }): ReactEleme
     const classes = useTweetImageStyles();
     const location = useLocation();
     const isModal = location.pathname.includes(MODAL);
+    const { popoverId, anchorEl, openPopover, handleOpenPopup, handleClosePopup } = usePopup();
 
     return (
-        <Link to={{ pathname: `${MODAL}/${tweetId}`, state: { background: location } }}>
-            <div id={"tweetImage"} className={classes.image}>
+        <div id={"tweetImage"} className={classes.image}>
+            <Link to={{ pathname: `${MODAL}/${tweetId}`, state: { background: location } }}>
                 <img className={isModal ? "small" : ""} src={imageSrc} alt={imageSrc} />
+            </Link>
+            <div className={classes.altButton} onClick={handleOpenPopup}>
+                ALT
             </div>
-        </Link>
+            <Popover
+                id={popoverId}
+                open={openPopover}
+                anchorEl={anchorEl}
+                onClose={handleClosePopup}
+                classes={{ paper: classes.popover }}
+                anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+                transformOrigin={{ vertical: "top", horizontal: "left" }}
+            >
+                <div className={classes.popoverContainer}>
+                    <Typography variant={"h3"} component={"div"}>
+                        Image description
+                    </Typography>
+                    <Typography variant={"subtitle1"} component={"div"}>
+                        Image description
+                    </Typography>
+                    <Button
+                        onClick={handleClosePopup}
+                        color="primary"
+                        variant="outlined"
+                        size="large"
+                        fullWidth
+                    >
+                        Dismiss
+                    </Button>
+                </div>
+            </Popover>
+        </div>
     );
 });
 
---
+++ frontend/src/hook/usePopup.ts
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+interface UsePopup {
+    anchorEl: HTMLElement | null,
+    handleOpenPopup: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void,
+    handleClosePopup: () => void,
+    popoverId: string | undefined,
+    openPopover: boolean
+}
+
+export const usePopup = (): UsePopup => {
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const openPopover = Boolean(anchorEl);
+    const popoverId = openPopover ? "simple-popover" : undefined;
+
+    const handleOpenPopup = (event: React.MouseEvent<HTMLDivElement, MouseEvent>): void => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClosePopup = (): void => {
+        setAnchorEl(null);
+    };
+
+    return { popoverId, anchorEl, openPopover, handleOpenPopup, handleClosePopup };
+};
