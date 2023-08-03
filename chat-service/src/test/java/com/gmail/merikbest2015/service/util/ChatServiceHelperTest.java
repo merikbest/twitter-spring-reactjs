1a88b534b4497093ac5c04de8d743d9c526023af
merikbest <merikbest2015@gmail.com>
2023-08-03 22:11:18 +0300

    Added ChatServiceHelper tests.

---
+++ chat-service/src/test/java/com/gmail/merikbest2015/service/util/ChatServiceHelperTest.java
@@ -0,0 +1,84 @@
+package com.gmail.merikbest2015.service.util;
+
+import com.gmail.merikbest2015.dto.response.chat.ChatTweetResponse;
+import com.gmail.merikbest2015.dto.response.chat.ChatUserParticipantResponse;
+import com.gmail.merikbest2015.exception.ApiRequestException;
+import com.gmail.merikbest2015.feign.TweetClient;
+import com.gmail.merikbest2015.feign.UserClient;
+import com.gmail.merikbest2015.util.TestConstants;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static com.gmail.merikbest2015.constants.ErrorMessage.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class ChatServiceHelperTest {
+
+    @Autowired
+    private ChatServiceHelper chatServiceHelper;
+
+    @MockBean
+    private UserClient userClient;
+
+    @MockBean
+    private TweetClient tweetClient;
+
+    @Test
+    public void getChatParticipant() {
+        when(userClient.getChatParticipant(TestConstants.USER_ID)).thenReturn(new ChatUserParticipantResponse());
+        assertEquals(new ChatUserParticipantResponse(), chatServiceHelper.getChatParticipant(TestConstants.USER_ID));
+        verify(userClient, times(1)).getChatParticipant(TestConstants.USER_ID);
+    }
+
+    @Test
+    public void getChatTweet() {
+        when(tweetClient.getChatTweet(TestConstants.TWEET_ID)).thenReturn(new ChatTweetResponse());
+        assertEquals(new ChatTweetResponse(), chatServiceHelper.getChatTweet(TestConstants.TWEET_ID));
+        verify(tweetClient, times(1)).getChatTweet(TestConstants.TWEET_ID);
+    }
+
+    @Test
+    public void isParticipantBlocked() {
+        when(userClient.isUserBlockedByMyProfile(TestConstants.USER_ID)).thenReturn(false);
+        when(userClient.isMyProfileBlockedByUser(1L)).thenReturn(true);
+        ApiRequestException exception = assertThrows(ApiRequestException.class,
+                () -> chatServiceHelper.isParticipantBlocked(TestConstants.CHAT_ID, 1L));
+        assertEquals(CHAT_PARTICIPANT_BLOCKED, exception.getMessage());
+        assertEquals(HttpStatus.BAD_REQUEST, exception.getStatus());
+    }
+
+    @Test
+    public void checkChatMessageLength() {
+        ApiRequestException exception = assertThrows(ApiRequestException.class,
+                () -> chatServiceHelper.checkChatMessageLength(""));
+        assertEquals(INCORRECT_CHAT_MESSAGE_LENGTH, exception.getMessage());
+        assertEquals(HttpStatus.BAD_REQUEST, exception.getStatus());
+    }
+
+    @Test
+    public void isTweetExists() {
+        when(tweetClient.isTweetExists(TestConstants.TWEET_ID)).thenReturn(false);
+        ApiRequestException exception = assertThrows(ApiRequestException.class,
+                () -> chatServiceHelper.isTweetExists(TestConstants.TWEET_ID));
+        assertEquals(TWEET_NOT_FOUND, exception.getMessage());
+        assertEquals(HttpStatus.NOT_FOUND, exception.getStatus());
+    }
+
+    @Test
+    public void isUserExists() {
+        when(userClient.isUserExists(TestConstants.USER_ID)).thenReturn(false);
+        ApiRequestException exception = assertThrows(ApiRequestException.class,
+                () -> chatServiceHelper.isUserExists(TestConstants.USER_ID));
+        assertEquals(USER_NOT_FOUND, exception.getMessage());
+        assertEquals(HttpStatus.NOT_FOUND, exception.getStatus());
+    }
+}
