d6da4723d989cf869c2c7dcca46d3c2509ee10d1
merikbest <merikbest2015@gmail.com>
2023-11-02 23:44:50 +0200

    Added RegistrationServiceImpl tests.

--- user-service/src/test/java/com/gmail/merikbest2015/service/RegistrationServiceImplTest.java
+++ user-service/src/test/java/com/gmail/merikbest2015/service/RegistrationServiceImplTest.java
@@ -1,9 +1,13 @@
 package com.gmail.merikbest2015.service;
 
+import com.gmail.merikbest2015.UserServiceTestHelper;
 import com.gmail.merikbest2015.amqp.AmqpProducer;
+import com.gmail.merikbest2015.dto.request.EmailRequest;
 import com.gmail.merikbest2015.dto.request.RegistrationRequest;
+import com.gmail.merikbest2015.exception.ApiRequestException;
 import com.gmail.merikbest2015.model.User;
 import com.gmail.merikbest2015.repository.UserRepository;
+import com.gmail.merikbest2015.repository.projection.UserCommonProjection;
 import com.gmail.merikbest2015.security.JwtProvider;
 import com.gmail.merikbest2015.service.util.UserServiceHelper;
 import com.gmail.merikbest2015.util.AbstractAuthTest;
@@ -11,12 +15,16 @@ import com.gmail.merikbest2015.util.TestConstants;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
 
+import java.util.Map;
 import java.util.Optional;
 
+import static com.gmail.merikbest2015.constants.ErrorMessage.EMAIL_HAS_ALREADY_BEEN_TAKEN;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 public class RegistrationServiceImplTest extends AbstractAuthTest {
@@ -38,6 +46,7 @@ public class RegistrationServiceImplTest extends AbstractAuthTest {
 
     @MockBean
     private AmqpProducer amqpProducer;
+    BindingResult bindingResult = mock(BindingResult.class);
 
     @Test
     public void registration_ShouldReturnSuccessMessage() {
@@ -50,7 +59,6 @@ public class RegistrationServiceImplTest extends AbstractAuthTest {
         request.setEmail(TestConstants.USER_EMAIL);
         request.setUsername(TestConstants.USERNAME);
         request.setBirthday(TestConstants.BIRTHDAY);
-        BindingResult bindingResult = mock(BindingResult.class);
         when(userRepository.getUserByEmail(TestConstants.USER_EMAIL, User.class)).thenReturn(Optional.empty());
         assertEquals("User data checked.", registrationService.registration(request, bindingResult));
         verify(userRepository, times(1)).getUserByEmail(TestConstants.USER_EMAIL, User.class);
@@ -69,10 +77,50 @@ public class RegistrationServiceImplTest extends AbstractAuthTest {
         request.setEmail(TestConstants.USER_EMAIL);
         request.setUsername(TestConstants.USERNAME);
         request.setBirthday(TestConstants.BIRTHDAY);
-        BindingResult bindingResult = mock(BindingResult.class);
         when(userRepository.getUserByEmail(TestConstants.USER_EMAIL, User.class)).thenReturn(Optional.of(user));
         assertEquals("User data checked.", registrationService.registration(request, bindingResult));
         verify(userRepository, times(1)).getUserByEmail(TestConstants.USER_EMAIL, User.class);
         verify(userRepository, times(1)).save(user);
+    }
+
+    @Test
+    public void registrationExistedUser_ShouldThrowEmailHasAlreadyBeenTakenException() {
+        User user = new User();
+        user.setEmail(TestConstants.USER_EMAIL);
+        user.setUsername(TestConstants.USERNAME);
+        user.setFullName(TestConstants.USERNAME);
+        user.setBirthday(TestConstants.BIRTHDAY);
+        user.setActive(true);
+        RegistrationRequest request = new RegistrationRequest();
+        request.setEmail(TestConstants.USER_EMAIL);
+        request.setUsername(TestConstants.USERNAME);
+        request.setBirthday(TestConstants.BIRTHDAY);
+        when(userRepository.getUserByEmail(TestConstants.USER_EMAIL, User.class)).thenReturn(Optional.of(user));
+        ApiRequestException exception = assertThrows(ApiRequestException.class,
+                () -> registrationService.registration(request, bindingResult));
+        assertEquals(EMAIL_HAS_ALREADY_BEEN_TAKEN, exception.getMessage());
+        assertEquals(HttpStatus.FORBIDDEN, exception.getStatus());
+    }
+
+    @Test
+    public void sendRegistrationCode_ShouldReturnSuccessMessage() {
+        UserCommonProjection userCommonProjection = UserServiceTestHelper.createUserCommonProjection();
+        EmailRequest request = EmailRequest.builder()
+                .to(userCommonProjection.getEmail())
+                .subject("Registration code")
+                .template("registration-template")
+                .attributes(Map.of(
+                        "fullName", userCommonProjection.getFullName(),
+                        "registrationCode", TestConstants.ACTIVATION_CODE))
+                .build();
+        when(userRepository.getUserByEmail(TestConstants.USER_EMAIL, UserCommonProjection.class))
+                .thenReturn(Optional.of(userCommonProjection));
+        when(userRepository.getActivationCode(userCommonProjection.getId()))
+                .thenReturn(TestConstants.ACTIVATION_CODE);
+        assertEquals("Registration code sent successfully",
+                registrationService.sendRegistrationCode(TestConstants.USER_EMAIL, bindingResult));
+        verify(userRepository, times(1)).getUserByEmail(TestConstants.USER_EMAIL, UserCommonProjection.class);
+        verify(userRepository, times(1)).getActivationCode(userCommonProjection.getId());
+        verify(amqpProducer, times(1)).sendEmail(request);
     }
 }
