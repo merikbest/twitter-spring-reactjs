759281dce78c7956c61fe9ecb4770047e80edc49
merikbest <merikbest2015@gmail.com>
2024-07-27 13:12:26 +0300

    Fixed UpdatePhoneNumberButton component

--- frontend/src/pages/Settings/Account/AccountInformation/ChangePhone/UpdatePhoneNumberButton/UpdatePhoneNumberButton.tsx
+++ frontend/src/pages/Settings/Account/AccountInformation/ChangePhone/UpdatePhoneNumberButton/UpdatePhoneNumberButton.tsx
@@ -1,23 +1,19 @@
 import React, { FC, ReactElement, useEffect } from "react";
 import { Typography } from "@material-ui/core";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 import { useModalWindow } from "../../../../../../hook/useModalWindow";
 import ChangePhoneModal from "./ChangePhoneModal/ChangePhoneModal";
-import { setUserLoadingStatus } from "../../../../../../store/ducks/user/actionCreators";
-import { LoadingStatus } from "../../../../../../types/common";
 import { selectUserIsLoaded } from "../../../../../../store/ducks/user/selectors";
 import { useUpdatePhoneNumberButtonStyles } from "./UpdatePhoneNumberButtonStyles";
 
 const UpdatePhoneNumberButton: FC = (): ReactElement => {
     const classes = useUpdatePhoneNumberButtonStyles();
-    const dispatch = useDispatch();
     const isUpdatedSuccess = useSelector(selectUserIsLoaded);
     const { visibleModalWindow, onOpenModalWindow, onCloseModalWindow } = useModalWindow();
 
     useEffect(() => {
         onCloseModalWindow();
-        dispatch(setUserLoadingStatus(LoadingStatus.NEVER));
     }, [isUpdatedSuccess]);
 
     return (
