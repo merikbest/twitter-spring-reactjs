ce4665ba2e9509029b825ce32b6dd515a003010c
merikbest <merikbest2015@gmail.com>
2024-09-04 19:25:18 +0300

    Fixed ImportUserDataJobConfiguration

--- lists-service/src/main/java/com/gmail/merikbest2015/batch/ImportUserDataJobConfiguration.java
+++ lists-service/src/main/java/com/gmail/merikbest2015/batch/ImportUserDataJobConfiguration.java
@@ -4,6 +4,7 @@ import com.gmail.merikbest2015.client.UserClient;
 import com.gmail.merikbest2015.event.UpdateUserEvent;
 import com.gmail.merikbest2015.model.User;
 import com.gmail.merikbest2015.service.UserHandlerService;
+import jakarta.persistence.EntityManagerFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -12,6 +13,8 @@ import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.database.JpaItemWriter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageImpl;
@@ -19,16 +22,20 @@ import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @RequiredArgsConstructor
-public class ImportUserDataJob {
+public class ImportUserDataJobConfiguration {
 
     private static final String JOB_NOME = "importUserData";
-    private final int chunkSize = 100;
-    private final int periodOfDays = 10;
+
+    @Value("${batch.chunkSize}")
+    private int chunkSize;
+
+    @Value("${batch.periodOfDays}")
+    private int periodOfDays;
 
     private final UserClient userClient;
     private final UserHandlerService userHandlerService;
-    private final JpaItemWriterFactory jpaItemWriterFactory;
     private final PlatformTransactionManager transactionManager;
+    private final EntityManagerFactory entityManagerFactory;
 
     @Bean(JOB_NOME)
     public Job importUserDataJob(JobRepository jobRepository) {
@@ -41,18 +48,26 @@ public class ImportUserDataJob {
     private Step importUserStep(JobRepository jobRepository) {
         return new StepBuilder("importUserStep", jobRepository)
                 .<UpdateUserEvent, User>chunk(chunkSize, transactionManager)
-                .reader(usersReader())
+                .reader(userItemReader())
                 .processor(userHandlerService::handleNewOrUpdateUser)
-                .writer(jpaItemWriterFactory.jpaItemWriter())
+                .writer(userItemWriter())
                 .build();
     }
 
     @Bean
     @StepScope
-    UserItemReader usersReader() {
+    UserItemReader userItemReader() {
         return new UserItemReader(pageable -> new PageImpl<>(
                 userClient.getBatchUsers(periodOfDays, pageable.getPageNumber(), pageable.getPageSize())),
                 chunkSize
         );
+    }
+
+    @Bean
+    @StepScope
+    JpaItemWriter<User> userItemWriter() {
+        JpaItemWriter<User> writer = new JpaItemWriter<>();
+        writer.setEntityManagerFactory(entityManagerFactory);
+        return writer;
     }
 }
