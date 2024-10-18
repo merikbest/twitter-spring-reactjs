f67f104f9c42b2e286769da93c78653a0be66c49
merikbest <merikbest2015@gmail.com>
2024-10-18 23:53:39 +0300

    Added action types and action creators

--- frontend/src/store/ducks/localization/actionCreators.ts
+++ frontend/src/store/ducks/localization/actionCreators.ts
@@ -1,10 +1,14 @@
 import {
     FetchCountryCodesActionInterface,
+    FetchGifImagesActionInterface,
     FetchLanguagesActionInterface,
+    FetchWallpapersActionInterface,
     LocalizationActionsType,
     SetCountryCodesActionInterface,
+    SetGifImagesActionInterface,
     SetLanguagesActionInterface,
-    SetLocalizationLoadingStateActionInterface
+    SetLocalizationLoadingStateActionInterface,
+    SetWallpapersActionInterface
 } from "./contracts/actionTypes";
 import { LocalizationState } from "./contracts/state";
 import { LoadingStatus } from "../../../types/common";
@@ -13,8 +17,16 @@ export const fetchCountryCodes = (): FetchCountryCodesActionInterface => ({
     type: LocalizationActionsType.FETCH_COUNTRY_CODES
 });
 
+export const fetchGifImages = (): FetchGifImagesActionInterface => ({
+    type: LocalizationActionsType.FETCH_GIF_IMAGES
+});
+
 export const fetchLanguages = (): FetchLanguagesActionInterface => ({
     type: LocalizationActionsType.FETCH_LANGUAGES
+});
+
+export const fetchWallpapers = (): FetchWallpapersActionInterface => ({
+    type: LocalizationActionsType.FETCH_WALLPAPERS
 });
 
 export const setCountryCodes = (payload: LocalizationState["countryCodes"]): SetCountryCodesActionInterface => ({
@@ -22,8 +34,18 @@ export const setCountryCodes = (payload: LocalizationState["countryCodes"]): Set
     payload
 });
 
+export const setGifImages = (payload: LocalizationState["gifImages"]): SetGifImagesActionInterface => ({
+    type: LocalizationActionsType.SET_GIF_IMAGES,
+    payload
+});
+
 export const setLanguages = (payload: LocalizationState["languages"]): SetLanguagesActionInterface => ({
     type: LocalizationActionsType.SET_LANGUAGES,
+    payload
+});
+
+export const setWallpapers = (payload: LocalizationState["wallpapers"]): SetWallpapersActionInterface => ({
+    type: LocalizationActionsType.SET_WALLPAPERS,
     payload
 });
 
--- frontend/src/store/ducks/localization/contracts/actionTypes.ts
+++ frontend/src/store/ducks/localization/contracts/actionTypes.ts
@@ -5,9 +5,13 @@ import { LoadingStatus } from "../../../../types/common";
 
 export enum LocalizationActionsType {
     FETCH_COUNTRY_CODES = "localization/FETCH_COUNTRY_CODES",
+    FETCH_GIF_IMAGES = "localization/FETCH_GIF_IMAGES",
     FETCH_LANGUAGES = "localization/FETCH_LANGUAGES",
+    FETCH_WALLPAPERS = "localization/FETCH_WALLPAPERS",
     SET_COUNTRY_CODES = "localization/SET_COUNTRY_CODES",
+    SET_GIF_IMAGES = "localization/SET_GIF_IMAGES",
     SET_LANGUAGES = "localization/SET_LANGUAGES",
+    SET_WALLPAPERS = "localization/SET_WALLPAPERS",
     SET_LOADING_STATE = "localization/SET_LOADING_STATE",
 }
 
@@ -15,8 +19,16 @@ export interface FetchCountryCodesActionInterface extends Action<LocalizationAct
     type: LocalizationActionsType.FETCH_COUNTRY_CODES;
 }
 
+export interface FetchGifImagesActionInterface extends Action<LocalizationActionsType> {
+    type: LocalizationActionsType.FETCH_GIF_IMAGES;
+}
+
 export interface FetchLanguagesActionInterface extends Action<LocalizationActionsType> {
     type: LocalizationActionsType.FETCH_LANGUAGES;
+}
+
+export interface FetchWallpapersActionInterface extends Action<LocalizationActionsType> {
+    type: LocalizationActionsType.FETCH_WALLPAPERS;
 }
 
 export interface SetCountryCodesActionInterface extends Action<LocalizationActionsType> {
@@ -24,9 +36,19 @@ export interface SetCountryCodesActionInterface extends Action<LocalizationActio
     payload: LocalizationState["countryCodes"];
 }
 
+export interface SetGifImagesActionInterface extends Action<LocalizationActionsType> {
+    type: LocalizationActionsType.SET_GIF_IMAGES;
+    payload: LocalizationState["gifImages"];
+}
+
 export interface SetLanguagesActionInterface extends Action<LocalizationActionsType> {
     type: LocalizationActionsType.SET_LANGUAGES;
     payload: LocalizationState["languages"];
+}
+
+export interface SetWallpapersActionInterface extends Action<LocalizationActionsType> {
+    type: LocalizationActionsType.SET_WALLPAPERS;
+    payload: LocalizationState["wallpapers"];
 }
 
 export interface SetLocalizationLoadingStateActionInterface extends Action<LocalizationActionsType> {
@@ -36,5 +58,7 @@ export interface SetLocalizationLoadingStateActionInterface extends Action<Local
 
 export type LocalizationActions =
     | SetCountryCodesActionInterface
+    | SetGifImagesActionInterface
     | SetLanguagesActionInterface
+    | SetWallpapersActionInterface
     | SetLocalizationLoadingStateActionInterface;
