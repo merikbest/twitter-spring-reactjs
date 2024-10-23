37d546f36fdb6f29f2dd4a2caff9f6d909c9a5cd
merikbest <merikbest2015@gmail.com>
2024-10-23 23:57:18 +0300

    Fixed GifTopics component

--- frontend/src/components/AddTweetForm/GifIconButton/GifModalWindow/GifTopics/__tests__/GifTopics.test.tsx
+++ frontend/src/components/AddTweetForm/GifIconButton/GifModalWindow/GifTopics/__tests__/GifTopics.test.tsx
@@ -1,17 +1,50 @@
 import React from "react";
 import { ImageListItem } from "@material-ui/core";
 
-import { createMockRootState, mountWithStore } from "../../../../../../util/test-utils/test-helper";
-import { LoadingStatus } from "../../../../../../types/common";
 import GifTopics from "../GifTopics";
+import { createMockRootState, mockDispatch, mountWithStore } from "../../../../../../util/test-utils/test-helper";
+import { LoadingStatus } from "../../../../../../types/common";
+import { LocalizationActionsType } from "../../../../../../store/ducks/localization/contracts/actionTypes";
+import Spinner from "../../../../../Spinner/Spinner";
+import { gifImage } from "../../../../../../util/test-utils/mock-test-data";
 
 describe("GifTopics", () => {
-    it("should render correctly", () => {
-        const mockRootState = createMockRootState(LoadingStatus.LOADED);
+    const mockRootState = createMockRootState(LoadingStatus.LOADING);
+    let mockDispatchFn: jest.Mock;
+
+    beforeEach(() => {
+        mockDispatchFn = mockDispatch();
+    });
+
+    it("should render loading spinner", () => {
+        const wrapper = mountWithStore(<GifTopics onClickGifTopic={jest.fn()} />, mockRootState);
+
+        expect(wrapper.find(Spinner).exists()).toBe(true);
+
+        expect(mockDispatchFn).nthCalledWith(1, { type: LocalizationActionsType.FETCH_GIF_IMAGES });
+    });
+
+    it("should render gif images items and click gif topic", () => {
+        const mockState = {
+            ...mockRootState,
+            localization: {
+                ...mockRootState,
+                gifImages: gifImage,
+                loadingState: LoadingStatus.LOADED
+            }
+        };
         const mockOnClickGifTopic = jest.fn();
-        const wrapper = mountWithStore(<GifTopics onClickGifTopic={mockOnClickGifTopic} />, mockRootState);
-        expect(wrapper.find(ImageListItem).length).toEqual(10);
+        const wrapper = mountWithStore(<GifTopics onClickGifTopic={mockOnClickGifTopic} />, mockState);
+
         wrapper.find(ImageListItem).at(0).simulate("click");
+
         expect(mockOnClickGifTopic).toHaveBeenCalled();
+        expect(wrapper.find(ImageListItem).length).toEqual(5);
+    });
+
+    it("should reset GifTopics", () => {
+        const wrapper = mountWithStore(<GifTopics onClickGifTopic={jest.fn()} />, mockRootState);
+        wrapper.unmount();
+        expect(mockDispatchFn).nthCalledWith(2, { type: LocalizationActionsType.RESET_LOCALIZATION_STATE });
     });
 });
